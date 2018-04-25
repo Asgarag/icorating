ca6c6928d380d0b025c8e2080f04a33c48604e74
Andrey <kozinandrew1@gmail.com>
2018-04-25 17:34:03 +0300

    Создал проект

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+//= parts/jquery-3.3.1.min.js
+//= parts/owl.carousel.min.js
+$(document).ready(function () {
+    //= parts/carousel.js
+	//= parts/menu.js
+});
---
+++ src/js/parts/menu.js
@@ -0,0 +1,6 @@
+$(".menu__switch").click(function() {
+	$(".menu__list").addClass("menu__list--active")
+});
+	$(".menu__close").click(function() {
+		$(".menu__list").removeClass("menu__list--active")
+});
