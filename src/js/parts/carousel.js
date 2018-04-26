399c33d4f9b72912a8906f01864abef9b39992f7
Andrey <kozinandrew1@gmail.com>
2018-04-26 17:34:19 +0300

    Сделал все кроме футера

--- src/js/parts/carousel.js
+++ src/js/parts/carousel.js
@@ -1,14 +1,20 @@
 $(".owl-carousel").owlCarousel({
-	items: 1,
-	autoplay: true,
+	items: 3,
 	loop: true,
+	dots: false,
+	center: true,
 	navText: ["",""],
 	responsiveClass: true,
 	responsive:{
 		0:{
+			items: 1,
 			nav: false
 		},
 		480:{
+			nav: true
+		},
+		768:{
+			items: 3,
 			nav: true
 		}
 	}
