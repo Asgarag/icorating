35dedb3af3c2aa43a3ec20c948781218c4619a60
Asgarag <kozinandrew1@gmail.com>
2018-04-27 21:12:29 +0300

    Доделал проект

--- src/js/parts/carousel.js
+++ src/js/parts/carousel.js
@@ -2,20 +2,16 @@ $(".owl-carousel").owlCarousel({
 	items: 3,
 	loop: true,
 	dots: false,
+	nav: true,
 	center: true,
 	navText: ["",""],
 	responsiveClass: true,
 	responsive:{
 		0:{
-			items: 1,
-			nav: false
+			items: 1
 		},
-		480:{
-			nav: true
-		},
-		768:{
-			items: 3,
-			nav: true
+		900:{
+			items: 3
 		}
 	}
 });
--- src/js/parts/menu.js
+++ src/js/parts/menu.js
@@ -4,3 +4,84 @@ $(".menu__switch").click(function() {
 	$(".menu__close").click(function() {
 		$(".menu__list").removeClass("menu__list--active")
 });
+
+$(".menu__link[href=\"#about\"]").click(function () {
+	event.preventDefault();
+	var header = $(".header").height();
+	var scrollTop = $('#about').offset().top - header;
+	$('body,html').animate({
+		scrollTop: scrollTop
+	}, 600);
+	$(".menu__list").removeClass("menu__list--active");
+});
+
+$(".menu__link[href=\"#token\"]").click(function () {
+	event.preventDefault();
+	var header = $(".header").height();
+	var scrollTop = $('#token').offset().top - header;
+	$('body,html').animate({
+		scrollTop: scrollTop
+	}, 600);
+	$(".menu__list").removeClass("menu__list--active");
+});
+
+$(".menu__link[href=\"#token-sale\"]").click(function () {
+	event.preventDefault();
+	var header = $(".header").height();
+	var scrollTop = $('#token-sale').offset().top - header;
+	$('body,html').animate({
+		scrollTop: scrollTop
+	}, 600);
+	$(".menu__list").removeClass("menu__list--active");
+});
+
+$(".menu__link[href=\"#plans\"]").click(function () {
+	event.preventDefault();
+	var header = $(".header").height();
+	var scrollTop = $('#plans').offset().top - header;
+	$('body,html').animate({
+		scrollTop: scrollTop
+	}, 600);
+	$(".menu__list").removeClass("menu__list--active");
+});
+
+$(".menu__link[href=\"#token-distribution\"]").click(function () {
+	event.preventDefault();
+	var header = $(".header").height();
+	var scrollTop = $('#token-distribution').offset().top - header;
+	$('body,html').animate({
+		scrollTop: scrollTop
+	}, 600);
+	$(".menu__list").removeClass("menu__list--active");
+});
+
+$(".menu__link[href=\"#funds-distribution\"]").click(function () {
+	event.preventDefault();
+	var header = $(".header").height();
+	var scrollTop = $('#funds-distribution').offset().top - header;
+	$('body,html').animate({
+		scrollTop: scrollTop
+	}, 600);
+	$(".menu__list").removeClass("menu__list--active");
+});
+
+$(".menu__link[href=\"#road\"]").click(function () {
+	event.preventDefault();
+	var header = $(".header").height();
+	var scrollTop = $('#road').offset().top - header;
+	$('body,html').animate({
+		scrollTop: scrollTop
+	}, 600);
+	$(".menu__list").removeClass("menu__list--active");
+});
+
+$(".menu__link[href=\"#team\"]").click(function () {
+	event.preventDefault();
+	var header = $(".header").height();
+	var scrollTop = $('#team').offset().top - header;
+	$('body,html').animate({
+		scrollTop: scrollTop
+	}, 600);
+	$(".menu__list").removeClass("menu__list--active");
+});
+
